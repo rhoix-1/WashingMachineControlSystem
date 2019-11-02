792699f8b9684bea3e2c4da8d63ced8ce6b4fa54
josepherSh <josepher.shunaula@gmail.com>
2019-11-01 20:58:06 -0400

    Selection Buttons Working Now

---
+++ v_0.4/main.py
@@ -0,0 +1,50 @@
+# Platt Tech Nasa Hunch Team (Ethan Feldman & Josepher Shunaula)
+import kivy
+import dataCollector
+from kivy.app import App
+from kivy.clock import Clock
+from datetime import datetime
+from kivy.lang import Builder
+from datetime import timedelta
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+''' 
+# Sends Data
+dataCollector.SendData()
+'''
+
+# This variable keeps track when you go through your choices
+selectionCounter = 0
+
+class HomeWindow(Screen):
+    # userInputSelections
+    #   This function is what allows you to select through you choices   
+    def userInputSelections(self, currentSelection, totalSelection):
+        global selectionCounter
+        totalSelection = eval(totalSelection)
+        if selectionCounter < len(totalSelection):
+            currentSelection.text = str(totalSelection[selectionCounter])
+            selectionCounter += 1
+        else:
+            selectionCounter = 0
+            currentSelection.text = str(totalSelection[selectionCounter])
+            selectionCounter += 1
+            
+
+class SettingsWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+
+class WashingMachineUI(App):
+    def build(self):
+        return
+
+
+if __name__ == "__main__":
+    WashingMachineUI().run()
+
